d8e0e06fe7fde72355d142274c69d3655856457c
Dima <aldmikon@gmail.com>
2024-06-18 11:14:18 +0800

    2nd commit Setting up the image2image pipeline

--- project/img2img.py
+++ project/img2img.py
@@ -1,23 +1,33 @@
-from diffusers import ControlNetModel,StableDiffusionControlNetPipeline,UniPCMultistepScheduler
-from diffusers.utils import load_image
+from diffusers import ControlNetModel,StableDiffusionControlNetImg2ImgPipeline,UniPCMultistepScheduler
+from diffusers.utils import load_image, make_image_grid
 import torch
+from PIL import Image
+
 
 
 
 controlnet = ControlNetModel.from_pretrained(
     "lllyasviel/sd-controlnet-depth", 
-    torch_dtype=torch.float16)
+    torch_dtype=torch.float16,
+    use_safetensors=True
+    )
 
-pipeline = StableDiffusionControlNetPipeline.from_pretrained(
+pipeline = StableDiffusionControlNetImg2ImgPipeline.from_pretrained(
     "lykon/dreamshaper-8", 
     controlnet=controlnet,
     torch_dtype=torch.float16, 
-    safety_checker=None)
+    use_safetensors=True
+    )
 
-#pipeline.to("cuda")
+#device = "cuda" if torch.cuda.is_available() else "cpu"
+device = "cpu"
+pipeline.to(device)
 
+# speed up diffusion process with faster scheduler and memory optimization
 pipeline.scheduler = UniPCMultistepScheduler.from_config(pipeline.scheduler.config)
+#pipeline.enable_model_cpu_offload()
 
+# Insert IP-Adapter into the pipeline
 pipeline.load_ip_adapter(
     "h94/IP-Adapter", 
     subfolder="models", 
@@ -26,18 +36,17 @@ pipeline.load_ip_adapter(
 
 # Define the prompt
 user_prompt = "masterpiece, portrait of a person, anime style, high quality, RAW photo, 8k uhd"
-
 # Load user image
-user_image = load_image("gallery/upwork_copy.png")
+user_image = load_image("https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/ip_adapter_bear_1.png")
+generator = torch.Generator(device=device).manual_seed(33)
 
-generator = torch.Generator(device="cpu").manual_seed(33)
 
-result = pipeline(
-    prompt=user_prompt,
-    image=user_image,
-    num_inference_steps=50,
-    generator=generator,
-    ).images[0]
+result_image = pipeline(
+prompt=user_prompt,
+num_inference_steps=50,
+generator=generator,
+image=user_image,
+).images[0]
 
-result.show()
-result.save("gallery/1.png")
+output = make_image_grid([user_image, result_image], rows=1, cols=2)
+output.show()
